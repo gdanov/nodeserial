631897ceca61d89b8f08e735b8ed8fdd521e864a
Georgi Danov <georgi.danov@gmail.com>
2021-12-26 19:23:41 +0100

    tty port as arg now

--- index.js
+++ index.js
@@ -1,67 +1,72 @@
-const SerialPort = require("serialport");
-const xesp = require("xespruino");
+const SerialPort = require('serialport');
+const xesp = require('xespruino');
 
-const portName = "/dev/cu.usbserial-0001";
+const { hideBin } = require('yargs/helpers');
+const yargs = require('yargs/yargs');
+const argv = yargs(hideBin(process.argv)).alias('p', 'port').argv;
+
+//console.log('>>>>>>', argv);
+let tty_port = argv.port || argv._[0];
+console.log('using TTY port ', tty_port);
+
+const portName = tty_port; //'/dev/tty.SLAB_USBtoUART'
 const portBaud = 115200;
 
 const myPort = new SerialPort(portName, { baudRate: portBaud });
 
 let parser = new SerialPort.parsers.Readline(); // make a new parser to read ASCII lines
-myPort
-	.pipe(parser)
+myPort.pipe(parser);
 //.pipe (new SerialPort.parsers.Ready ({delimiter: String.fromCharCode (...[ 27, 91, 63, 55, 108 ])}));
 
 function showPortOpen() {
-  console.log("port open. Data rate: " + myPort.baudRate);
-  console.log("-------------");
+  console.log('port open. Data rate: ' + myPort.baudRate);
+  console.log('-------------');
 }
 
 function readSerialData(data) {
-	//  console.log(data.charCodeAt (0), (new TextEncoder ()).encode (data), "|\n", data);
-	console.log (data)
+  //  console.log(data.charCodeAt (0), (new TextEncoder ()).encode (data), "|\n", data);
+  console.log(data);
 
-	if (data == String.fromCharCode (...[ 27, 91, 63, 55, 108, 13 ])){
-		// better detect ready
-		myPort.emit ("ready")
-	}
-	
-  if (data.startsWith("|____|___|  _|_| |___|_|_|_|___|")) {
+  if (data == String.fromCharCode(...[27, 91, 63, 55, 108, 13])) {
+    // better detect ready
+    myPort.emit('ready');
+  }
+
+  if (data.startsWith('|____|___|  _|_| |___|_|_|_|___|')) {
     // detect restarted
   }
 }
 
 function showPortClose() {
-  console.log("port closed.");
+  console.log('port closed.');
 }
 
 function showError(error) {
-  console.log("Serial port error: " + error);
+  console.log('Serial port error: ' + error);
 }
 
-myPort.on("open", showPortOpen);
-parser.on("data", readSerialData);
-myPort.on("close", showPortClose);
-myPort.on("error", showError);
+myPort.on('open', showPortOpen);
+parser.on('data', readSerialData);
+myPort.on('close', showPortClose);
+myPort.on('error', showError);
 
 let onReady = null;
 
-myPort.on ("ready",()=>{
-	console.log ('READY?')
-	if (onReady){
-		onReady ();
-		onReady = null;
-	}
-})
+myPort.on('ready', () => {
+  console.log('READY?');
+  if (onReady) {
+    onReady();
+    onReady = null;
+  }
+});
 
-process.stdin
-  .pipe(require("split")("\n", null, { maxLength: 999999 }))
-  .on("data", processStdinLine);
+process.stdin.pipe(require('split')('\n', null, { maxLength: 999999 })).on('data', processStdinLine);
 
 // ESC[8m 	ESC[28m 	TTY set hidden/invisible mode
 const hide_start = String.fromCharCode(...[27, 91, 56, 109]);
 const hide_end = String.fromCharCode(...[27, 91, 50, 56, 109]);
 let accumulating = false;
-let accumulator = "";
+let accumulator = '';
 
 const test = true;
 
@@ -69,7 +74,7 @@ function processStdinLine(line) {
   // we can have single-line code push so this is why code is structured this way
   if (line.startsWith(hide_start)) {
     accumulating = true;
-    accumulator = "";
+    accumulator = '';
   }
 
   if (accumulating) {
@@ -82,24 +87,27 @@ function processStdinLine(line) {
     }
     accumulator += atob(todo);
   } else {
-    myPort.write(line + "\n");
+    myPort.write(line + '\n');
   }
 
   if (line.endsWith(hide_end)) {
     accumulating = false;
     //console.log(">>>", accumulator, "<<<");
 
-		onReady = ()=>{
-			xesp.transformCode(accumulator, function (code) {
-				myPort.write (code.split ("\n")
-											// you can use '\x10' literal but prettier craps it out replacing the quotes
-											.map (line => String.fromCharCode (0x10) + line)
-											.join ("\n"))
-				myPort.drain();
-				console.log ("###### ready ######")
-			});	
-		}
-		
-		myPort.write ("reset();\n");
+    onReady = () => {
+      xesp.transformCode(accumulator, function (code) {
+        myPort.write(
+          code
+            .split('\n')
+            // you can use '\x10' literal but prettier craps it out replacing the quotes
+            .map((line) => String.fromCharCode(0x10) + line)
+            .join('\n')
+        );
+        myPort.drain();
+        console.log('###### ready ######');
+      });
+    };
+
+    myPort.write('reset();\n');
   }
 }
